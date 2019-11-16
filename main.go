e10de67e411ef6bebb30b8d44438d92e80747772
Lerman <contact@lerman.fr>
2019-11-16 20:36:28 +0100

    sql connection + viper

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"regexp"
+	"strconv"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/hlerman/fflogs-discord-bot/users"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	viper.SetConfigName("settings")
+	viper.AddConfigPath(".")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	dg, err := discordgo.New("Bot " + viper.GetString("token"))
+	if err != nil {
+		log.Fatal("error creating Discord session,", err)
+	}
+
+	// Register the messageCreate func as a callback for MessageCreate events.
+	dg.AddHandler(messageCreate)
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		log.Fatal("error opening connection,", err)
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	log.Info("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the autenticated bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	// If the message is "ping" reply with "Pong!"
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+
+	// If the message is "pong" reply with "Ping!"
+	if m.Content == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
+
+	if m.Content[:13] == "!addCharacter" {
+		r := regexp.MustCompile("^!addCharacter ([0-9]+)$")
+		match := r.FindStringSubmatch(m.Content)
+		if len(match) > 0 {
+			//DEBUG : s.ChannelMessageSend(.ChannelID, match[1])
+			id, _ := strconv.Atoi(match[1])
+			users.AddCharacter(m, s, id)
+		}
+	}
+}
--- users/users.go
+++ users/users.go
@@ -1,18 +1,36 @@
 package users
 
 import (
+	"database/sql"
+	"fmt"
+	"strconv"
+
 	"github.com/bwmarrin/discordgo"
+	_ "github.com/go-sql-driver/mysql"
+	log "github.com/sirupsen/logrus"
 )
 
-func addUser(id string) error {
-	return nil
+func connect() *sql.DB {
+	db, err := sql.Open("mysql", "root:@/fflogs-discord-bot")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db
 }
 
-func AddCharacter(m *discordgo.MessageCreate, s *discordgo.Session, characterId int) string {
+func AddCharacter(m *discordgo.MessageCreate, s *discordgo.Session, characterId int) {
+	fmt.Println(isUserAlreadyExist(m.Author.ID))
 	if isUserAlreadyExist(m.Author.ID) == false {
 		err := addUser(m.Author.ID)
 		if err != nil {
-			return "Oups, je n'arrive pas à vous ajouter"
+			s.ChannelMessageSend(m.ChannelID, "Oups, je n'arrive pas à vous ajouter")
+			return
 		}
 
 		s.ChannelMessageSend(m.ChannelID, "Nous ne nous sommes jamais vu non ? Je vous enregistre")
@@ -22,12 +40,60 @@ func AddCharacter(m *discordgo.MessageCreate, s *discordgo.Session, characterId 
 	if isCharacterAlreadyExist() == true {
 		s.ChannelMessageSend(m.ChannelID, "Votre personnage est déjà suivi")
 	}
+}
 
-	return ""
+func addUser(id string) error {
+	db, err := sql.Open("mysql", "root:@/fflogs-discord-bot")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stmt, err := db.Prepare("INSERT INTO users VALUES( ? )")
+	if err != nil {
+		log.Warn(err)
+		return err
+	}
+	defer stmt.Close()
+
+	i, err := strconv.Atoi(id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = stmt.Exec(i)
+	if err != nil {
+		log.Warn(err)
+		return err
+	}
+
+	return nil
 }
 
 func isUserAlreadyExist(id string) bool {
-	return false
+	db := connect()
+	defer db.Close()
+
+	i, err := strconv.Atoi(id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var discordID int
+	err = db.QueryRow("SELECT discord_id FROM users WHERE discord_id = ?", i).Scan(&discordID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return false
+		} else {
+			log.Fatal(err)
+		}
+	}
+
+	return true
 }
 
 func isCharacterAlreadyExist() bool {
